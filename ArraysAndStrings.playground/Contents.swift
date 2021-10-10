425311fd35c7bf21f3726873528904fc8161bc39
gracedmarks <rphanchotong@gmail.com>
2021-10-10 13:52:42 +0700

    Implement palindrome

--- ArraysAndStrings.playground/Contents.swift
+++ ArraysAndStrings.playground/Contents.swift
@@ -112,6 +112,34 @@ print("True string length: \(urlifiedOutput.strLen)")
  Hints:#106,#121,#134,#136
  */
 
+func isPalindrome(string: String) -> Bool {
+
+    guard string.isEmpty == false else { return false }
+
+    // tac o cat
+    // check in pairs
+    var leftIndex: String.Index = string.index(string.startIndex, offsetBy: 0) // first element
+    var rightIndex: String.Index = string.index(string.endIndex, offsetBy: -1) // before nil
+
+    for _ in 1...(string.count / 2) {
+        
+        print("Comparing \(string[leftIndex]) and \(string[rightIndex])")
+
+        if string[leftIndex] != string[rightIndex] {
+            return false
+        }
+        
+        leftIndex = string.index(after: leftIndex)
+        rightIndex = string.index(before: rightIndex)
+    }
+
+    return true
+}
+
+var palindromeString = "tcact"
+var isPalindrome: Bool = isPalindrome(string: palindromeString)
+print("Is \(palindromeString) palindrome: \(isPalindrome)")
+
 
 
 /*

5eebbfaa37b9eb6ef0c6b024d07b7fcbf02f9200
gracedmarks <rphanchotong@gmail.com>
2021-11-20 16:42:14 +0700

    Update Node-Depths.swift

--- Node-Depths.swift
+++ Node-Depths.swift
@@ -1,4 +1,4 @@
-// MARK: - FIND SUM OF NODE DEPTHS
+// MARK: - FIND SUM OF THE NODE DEPTHS
 
 class BinaryTree {
     var value: Int
@@ -11,6 +11,21 @@ class BinaryTree {
     
 }
 
+// MARK: - SOLUTION 1
+
+static func nodeDepths(_ root: BinaryTree?) -> Int {
+    return getSum(root: root, depth: 0)
+}
+
+static func getSum(root: BinaryTree?, depth: Int) -> Int {
+    guard let root = root else { return 0 }
+    let sumLeft = getSum(root: root.left, depth: depth + 1)
+    let sumRight = getSum(root: root.right, depth: depth + 1)
+    return sumLeft + sumRight + depth
+}
+
+
+// MARK: - SOLUTION 2
 static func nodeDepths(_ root: BinaryTree?) -> Int {
     guard let root = root else { return 0 }
     return getSum(root: root, depth: 1)
